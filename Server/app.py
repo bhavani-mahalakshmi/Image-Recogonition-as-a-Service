8ea147a02782483ab665185d461ce65c5c300099
Srikanth Kini <kini007@Srikanths-MacBook-Air.local>
2022-03-10 18:01:52 -0700

    Added workload generators and modified app.py

--- Server/app.py
+++ Server/app.py
@@ -5,9 +5,7 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-sqs = boto3.client('sqs', region_name='us-east-1',
-                    aws_access_key_id="ACCESS_KEY_ID", 
-                    aws_secret_access_key="SECRET_ACCESS_KEY")
+sqs = boto3.client('sqs', region_name='us-east-1')
 
 queue_url = 'https://sqs.us-east-1.amazonaws.com/051675418934/Input-Image-Queue.fifo'
 
@@ -21,15 +19,25 @@ def home():
 
 @app.route('/', methods=['POST'])
 def upload():
-    for uploaded_file in request.files.getlist('file'):
-        if uploaded_file.filename:
-            with open(uploaded_file.filename, "rb") as image_to_string:
-                converted_string = base64.b64encode(image_to_string.read())
-                sqs_message_body = {"image_string": converted_string,
-                                    "file_name": uploaded_file.filename}
+    try:
+        for uploaded_file in request.files.getlist('file'):
+            if uploaded_file.filename:
+                converted_string = base64.b64encode(uploaded_file.read())
+                sqs_message_body = uploaded_file.filename + ";" + str(converted_string, "utf-8")
                 # Send message to SQS queue.
                 sqs.send_message(QueueUrl=queue_url, MessageBody=sqs_message_body, MessageGroupId='messageGroup1')
-    return redirect(url_for('index'))
+        return redirect(url_for('home'))
+    except Exception as e:
+        print(e)
+
+@app.route('/results', methods=['GET'])
+def results():
+    s3_resource= boto3.resource('s3')
+    my_bucket = s3_resource.Bucket('imageoutputbucket')
+    results = dict()
+    for obj in my_bucket.objects.all():
+        results[obj.key] = obj.get()['Body'].read().decode('utf-8')
+    return render_template('results.html', results=results)
 
 if __name__ == '__main__':
     app.run(
