9d3dba9e93a1baace90ff138a5b532ad9b14722a
Srikanth Kini <kini007@Srikanths-MacBook-Air.local>
2022-03-11 17:13:26 -0700

    Modified app

--- Server/app.py
+++ Server/app.py
@@ -1,13 +1,16 @@
 import os
 import boto3
 import base64
-from flask import Flask, render_template, request, redirect, url_for
+import uuid
+import json
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 app = Flask(__name__)
 
 sqs = boto3.client('sqs', region_name='us-east-1')
 
-queue_url = 'https://sqs.us-east-1.amazonaws.com/051675418934/Input-Image-Queue.fifo'
+input_queue_url = 'https://sqs.us-east-1.amazonaws.com/051675418934/Input-Image-Queue.fifo'
+output_queue_url = 'https://sqs.us-east-1.amazonaws.com/051675418934/Output-Image-Queue.fifo'
 
 @app.route('/health-check')
 def health_check():
@@ -18,26 +21,26 @@ def home():
     return render_template('index.html')
 
 @app.route('/', methods=['POST'])
-def upload():
+def process():
     try:
-        for uploaded_file in request.files.getlist('file'):
-            if uploaded_file.filename:
-                converted_string = base64.b64encode(uploaded_file.read())
-                sqs_message_body = uploaded_file.filename + ";" + str(converted_string, "utf-8")
+        uploaded_file = request.files.get('myfile')
+        if uploaded_file.filename:
+            identifier = str(uuid.uuid4())
+            converted_string = base64.b64encode(uploaded_file.read())
+            sqs_message_body = {"encoded_image": str(converted_string, "utf-8"), "unique_id": identifier, "file_name": uploaded_file.filename}
                 # Send message to SQS queue.
-                sqs.send_message(QueueUrl=queue_url, MessageBody=sqs_message_body, MessageGroupId='messageGroup1')
-        return redirect(url_for('home'))
+            sqs.send_message(QueueUrl=input_queue_url, MessageBody=json.dumps(sqs_message_body), MessageGroupId='messageGroup1')
+        print("Uploaded file to SQS Input Queue!")
+        while True:
+            sqs_output = sqs.receive_message(QueueUrl=output_queue_url, AttributeNames=['All'], MaxNumberOfMessages=5, WaitTimeSeconds=1)
+            messages = sqs_output.get('Messages', [])
+            for item in messages:
+                msg_body = json.loads(item.get('Body'))
+                if msg_body['unique_id'] == identifier:
+                    sqs.delete_message(QueueUrl=output_queue_url, ReceiptHandle=item.get('ReceiptHandle'))
+                    return jsonify(msg_body.get('classification'))
     except Exception as e:
         print(e)
-
-@app.route('/results', methods=['GET'])
-def results():
-    s3_resource= boto3.resource('s3')
-    my_bucket = s3_resource.Bucket('imageoutputbucket')
-    results = dict()
-    for obj in my_bucket.objects.all():
-        results[obj.key] = obj.get()['Body'].read().decode('utf-8')
-    return render_template('results.html', results=results)
 
 if __name__ == '__main__':
     app.run(
